74b1c1a0ee8f7d1de57729288cae57e6769cddd5
HallexCosta <hallex.costa@hotmail.com>
2020-06-02 18:22:03 -0300

    Fixing the zinit-config installation directory

--- install.sh
+++ install.sh
@@ -8,20 +8,20 @@ ln -s $(pwd)/.zshrc ~/.zshrc
 echo "Installing binaries:"
 
 echo "Updating apt-get packages"
-echo <yourpassword> | command sudo apt-get update
+command sudo apt-get update
 
 #echo "Installing chrome"
 
 #echo "Installing VS Code"
 
 echo "Making Zsh the default shell"
-echo <yourpassword> | command chsh -s $(which zsh)
+command chsh -s $(which zsh)
 
 echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Installing NeoVim"
-echo | command sudo add-apt-repository ppa:neovim-ppa/stable
-echo <yourpassword> | command sudo apt-get update
+command sudo add-apt-repository ppa:neovim-ppa/stable
+command sudo apt-get update
 
 echo "Done!"
