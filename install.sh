1f223e7c3a6226d962d4706f53463fdc7e242721
HallexCosta <hallex.costa@hotmail.com>
2021-05-03 20:56:22 -0300

    chore(install): update

--- install.sh
+++ install.sh
@@ -1,14 +1,30 @@
 echo "Import envrionment variables"
+GIT_CONFIG_FILE=~/.gitconfig
+BIN_DIR=~/dotfiles/bin
+ENV_CONFIGS=~/configs/.env.sh
 SSH_EMAIL="hallex.costa@hotmail.com"
-#source ~/dotfiles/env.sh
-
-# VIM OR NEOVIM
-echo "Symlinking things:"
+#source ~/dotfiles/configs/env.sh
 
 # ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 # ln -s ~/dotfiles/.zshrc ~/.zshrc
 
+echo "Initializing auto-install..."
+source $BIN_DIR/binaries.sh
+source $BIN_DIR/nvm.sh
+source $BIN_DIR/yarn.sh
+source $BIN_DIR/nvim.sh
+source $BIN_DIR/ssh.sh
+source $BIN_DIR/oh-my-zsh.sh
+source $BIN_DIR/spaceship-theme.sh
+
 echo "Restarting .zshrc file..."
 source ~/.zshrc
+
+if [ -f "$GIT_CONFIG_FILE" ]; then
+  mv $GIT_CONFIG_FILE $GIT_CONFIG_FILE.orig
+  ln -s ~/dotfiles/.gitconfig $GIT_CONFIG_FILE 
+else
+  ln -s ~/dotfiles/.gitconfig $GIT_CONFIG_FILE
+fi
 
 echo "Done!"
