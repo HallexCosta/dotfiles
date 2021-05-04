2354a1bb3963f140b07012925b2e3b625949b3d9
HallexCosta <hallex.costa@hotmail.com>
2021-05-04 01:54:37 -0300

    chore(configs): add var ALIAS_FILE

--- configs/env.sh
+++ configs/env.sh
@@ -1,5 +1,6 @@
-GIT_CONFIG_FILE=~/.gitconfig
-ZSH_MODIFIED_FILE=~/.zshrc
-BIN_DIR=~/dotfiles/bin
-ENV_CONFIGS=~/configs/.env.sh
+GIT_CONFIG_FILE=$HOME/.gitconfig
+ZSHRC_FILE=$HOME/.zshrc
+ALIAS_FILE=$HOME/.alias
+BIN_DIR=$HOME/dotfiles/bin
+ENV_CONFIGS=$HOME/configs/.env.sh
 SSH_EMAIL="hallex.costa@hotmail.com"
