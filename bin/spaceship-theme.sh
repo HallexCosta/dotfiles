bd5ff51053f4f02cc7c05b27f917d663b31dee1c
HallexCosta <hallex.costa@hotmail.com>
2021-05-03 21:23:29 -0300

    chore(bins): remove creation of symbolinking for spaceship-theme

--- bin/spaceship-theme.sh
+++ bin/spaceship-theme.sh
@@ -1,7 +1,4 @@
 echo "Installing theme spaceship..."
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 
-echo "Create symbolikings spaceship theme..."
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
 echo "Done!"
