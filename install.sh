840611416e3b620b64be406324d3d27155735c19
HallexCosta <hallex.costa@hotmail.com>
2021-05-04 01:55:10 -0300

    chore(install): add load configs .env.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ BIN_DIR=~/dotfiles/bin
 ENV_CONFIGS=~/configs/.env.sh
 SSH_EMAIL="hallex.costa@hotmail.com"
 ZSH_MODIFIED_FILE=~/.zshrc
-#source ~/dotfiles/configs/env.sh
+source $HOME/dotfiles/configs/env.sh
 
 echo "Initializing auto-install..."
 source $BIN_DIR/binaries.sh
