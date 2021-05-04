05053f928333183920a6bb4c6bdcacba76f8b5bd
HallexCosta <hallex.costa@hotmail.com>
2021-05-04 01:31:47 -0300

    chore(configure): change local create symbolinking theme spaceship

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@ mv $ZSH_MODIFIED_FILE $ZSH_MODIFIED_FILE.orig
 ln -s ~/dotfiles/.gitconfig $ZSH_MODIFIED_FILE
 
 echo "Create symboliking spaceship theme..."
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
 
 echo "Create symboliking .gitconfig..."
 if [ -f "$GIT_CONFIG_FILE" ]; then
