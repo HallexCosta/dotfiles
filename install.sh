7e3f5be4e4dfdc21b9d8278e9df258df2c51e565
HallexCosta <hallex.costa@hotmail.com>
2021-05-03 21:29:44 -0300

    chore(install): add new var env and remove unuseless code

--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@ GIT_CONFIG_FILE=~/.gitconfig
 BIN_DIR=~/dotfiles/bin
 ENV_CONFIGS=~/configs/.env.sh
 SSH_EMAIL="hallex.costa@hotmail.com"
+ZSH_MODIFIED_FILE=~/.zshrc
 #source ~/dotfiles/configs/env.sh
-
-# ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-# ln -s ~/dotfiles/.zshrc ~/.zshrc
 
 echo "Initializing auto-install..."
 source $BIN_DIR/binaries.sh
@@ -19,12 +17,5 @@ source $BIN_DIR/spaceship-theme.sh
 
 echo "Restarting .zshrc file..."
 source ~/.zshrc
-
-if [ -f "$GIT_CONFIG_FILE" ]; then
-  mv $GIT_CONFIG_FILE $GIT_CONFIG_FILE.orig
-  ln -s ~/dotfiles/.gitconfig $GIT_CONFIG_FILE 
-else
-  ln -s ~/dotfiles/.gitconfig $GIT_CONFIG_FILE
-fi
 
 echo "Done!"
