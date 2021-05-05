a7d5272642842c75031d11d4b5b55129fcee563c
HallexCosta <hallex.costa@hotmail.com>
2021-05-05 00:26:44 -0300

    chore(tools): add node-and-npm install and import env variables

--- tools.sh
+++ tools.sh
@@ -1,9 +1,5 @@
 echo "Import envrionment variables"
-GIT_CONFIG_FILE=~/.gitconfig
-BIN_DIR=~/dotfiles/bin
-ENV_CONFIGS=~/configs/.env.sh
-SSH_EMAIL="hallex.costa@hotmail.com"
-#source ~/dotfiles/configs/env.sh
+source ~/dotfiles/configs/env.sh
 
 # ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 # ln -s ~/dotfiles/.zshrc ~/.zshrc
@@ -11,6 +7,7 @@ SSH_EMAIL="hallex.costa@hotmail.com"
 echo "Initializing auto-install tools.."
 source $BIN_DIR/binaries.sh
 source $BIN_DIR/nvm.sh
+source $BIN_DIR/node-and-npm.sh
 source $BIN_DIR/yarn.sh
 source $BIN_DIR/neovim.sh
 source $BIN_DIR/ssh.sh
