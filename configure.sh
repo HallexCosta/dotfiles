a2f9f4553d759084e0f2cc3117559ffdea32e718
HallexCosta <hallex.costa@hotmail.com>
2021-05-05 01:01:59 -0300

    chore(configure): fix dir for get .zshrc, .gitconfig and .alias file

--- configure.sh
+++ configure.sh
@@ -4,9 +4,9 @@ pip3 install psutil
 echo "Create symboliking .zshrc modified..."
 if [ -f "$ZSHRC_CONFIG_FILE" ]; then
   mv $ZSHRC_FILE $ZSHRC_FILE.orig
-  ln -s ~/dotfiles/.zshrc $ZSHRC_FILE
+  ln -s ~/dotfiles/configs/.zshrc $ZSHRC_FILE
 else
-  ln -s ~/dotfiles/.zshrc $ZSHRC_FILE
+  ln -s ~/dotfiles/configs/.zshrc $ZSHRC_FILE
 fi
 
 echo "Create symboliking spaceship theme..."
@@ -15,17 +15,17 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spa
 echo "Create symboliking .gitconfig..."
 if [ -f "$GIT_CONFIG_FILE" ]; then
   mv $GIT_CONFIG_FILE $GIT_CONFIG_FILE.orig
-  ln -s ~/dotfiles/.gitconfig $GIT_CONFIG_FILE 
+  ln -s ~/dotfiles/configs/.gitconfig $GIT_CONFIG_FILE 
 else
-  ln -s ~/dotfiles/.gitconfig $GIT_CONFIG_FILE
+  ln -s ~/dotfiles/configs/.gitconfig $GIT_CONFIG_FILE
 fi
 
 echo "Create symboliking .alias..."
 if [ -f "$ALIAS_FILE" ]; then
   mv $ALIAS_FILE $ALIAS_FILE.orig
-  ln -s ~/dotfiles/.zshrc $ALIAS_FILE
+  ln -s ~/dotfiles/configs/.alias $ALIAS_FILE
 else
-  ln -s ~/dotfiles/.zshrc $ALIAS_FILE
+  ln -s ~/dotfiles/configs/.alias $ALIAS_FILE
 fi
 
 echo "Done!"
