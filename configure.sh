94c80947abb49ba2b925c80ea0fc5400a0bda7b4
HallexCosta <hallex.costa@hotmail.com>
2021-05-07 19:17:54 -0300

    chore(configure): fix variables names

--- configure.sh
+++ configure.sh
@@ -1,12 +1,15 @@
+echo "Import envrionment variables"
+source ./configs/.env
+
 echo "Installing psutil for zshrc..."
 pip3 install psutil
 
 echo "Create symboliking .zshrc modified..."
-if [ -f "$ZSHRC_CONFIG_FILE" ]; then
+if [ -f "$ZSHRC_FILE" ]; then
   mv $ZSHRC_FILE $ZSHRC_FILE.orig
-  ln -s ~/dotfiles/configs/.zshrc $ZSHRC_FILE
+  ln -s $ZSHRC_FILE_UPDATED $ZSHRC_FILE
 else
-  ln -s ~/dotfiles/configs/.zshrc $ZSHRC_FILE
+  ln -s $ZSHRC_FILE_UPDATED $ZSHRC_FILE
 fi
 
 echo "Create symboliking spaceship theme..."
@@ -15,17 +18,25 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spa
 echo "Create symboliking .gitconfig..."
 if [ -f "$GIT_CONFIG_FILE" ]; then
   mv $GIT_CONFIG_FILE $GIT_CONFIG_FILE.orig
-  ln -s ~/dotfiles/configs/.gitconfig $GIT_CONFIG_FILE 
+  ln -s $GIT_CONFIG_FILE_UPDATED $GIT_CONFIG_FILE 
 else
-  ln -s ~/dotfiles/configs/.gitconfig $GIT_CONFIG_FILE
+  ln -s $GIT_CONFIG_FILE_UPDATED $GIT_CONFIG_FILE
 fi
 
 echo "Create symboliking .alias..."
 if [ -f "$ALIAS_FILE" ]; then
-  mv $ALIAS_FILE $ALIAS_FILE.orig
-  ln -s ~/dotfiles/configs/.alias $ALIAS_FILE
+  mv $ALIAS_FILE_UPDATED $ALIAS_FILE.orig
+  ln -s $ALIAS_FILE_UPDATED $ALIAS_FILE
 else
-  ln -s ~/dotfiles/configs/.alias $ALIAS_FILE
+  ln -s $ALIAS_FILE_UPDATED $ALIAS_FILE
+fi
+
+echo "Create symboliking .tmux.conf..."
+if [ -f "$TMUX_FILE" ]; then
+  mv $TMUX_FILE $ALIAS_FILE.orig
+  ln -s $TMUX_FILE_UPDATED $TMUX_FILE
+else
+  ln -s $TMUX_FILE_UPDATED $TMUX_FILE
 fi
 
 echo "Done!"
