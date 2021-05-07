345c7d09e41d2fd9e22d9a513d1eb236de9c3e8e
HallexCosta <hallex.costa@hotmail.com>
2021-05-07 19:21:03 -0300

    chore(configs): fix directory imports tools, shell and shell-theme

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
-echo "Import envrionment variables"
-source ./configs/.env
-
 echo "Initializing auto-install tools, shell and shell-theme..."
-source $BINS_DIR/tools.sh
-source $BINS_DIR/shell.sh
-source $BINS_DIR/shell-theme.sh
+source ./tools.sh
+source ./shell.sh
+source ./shell-theme.sh
 
 echo "Done!"
