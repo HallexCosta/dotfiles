1c14e109021b246ffcc9815bd5cf6bf11555a548
Hállex da Silva Costa <hallex.costa@hotmail.com>
2021-05-10 04:14:34 +0000

    chore(install): load configure script

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 echo "Import envrionment variables"
 source ./configs/.env
 
-echo "Initializing auto-install tools, shell and shell-theme..."
+echo "Initializing auto-install tools, shell and shell-theme and configure..."
 source ./tools.sh
 source ./shell.sh
 source ./shell-theme.sh
+source ./configure.sh
 
 echo "Done!"
