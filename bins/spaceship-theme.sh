30aa546408e646dadbbd5134e6b9e95cecda0f48
HallexCosta <hallex.costa@hotmail.com>
2021-05-10 20:54:54 -0300

    chore(bins): use vars ZSH_DIR and ZSH_CUSTOM_DIR"

--- bins/spaceship-theme.sh
+++ bins/spaceship-theme.sh
@@ -1,6 +1,6 @@
 echo ""
 echo "Installing theme spaceship..."
-mkdir $OH_MY_ZSH_DIR/themes/spaceship-prompt
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$OH_MY_ZSH_DIR/themes/spaceship-prompt"
+mkdir $ZSH_CUSTOM_DIR/themes/spaceship-prompt
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM_DIR/themes/spaceship-prompt"
 
 echo "Done!"
