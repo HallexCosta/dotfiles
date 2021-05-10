e374fb77d5fdc2abf242efa4818c6f4513b72e89
Hállex da Silva Costa <hallex.costa@hotmail.com>
2021-05-10 21:05:47 +0000

    chore(bins): text to installing Neovim Sofi to Sofy

--- bins/neovim.sh
+++ bins/neovim.sh
@@ -1,7 +1,7 @@
 echo "Installing NeoVim..."
 sudo apt install neovim
 
-echo "Installing Neovim Sofi by Hállex..." 
+echo "Installing Neovim Sofy by Hállex..." 
 mkdir $HOME/.config/nvim
 git clone https://github.com/HallexCosta/nvim.git $HOME/.config/nvim
 
