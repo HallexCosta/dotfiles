4334bd494559142538016fe58a11997e0f2e3bf2
HallexCosta <hallex.costa@hotmail.com>
2021-05-10 20:39:27 -0300

    chore(configure): fix bad configuration

--- configure.sh
+++ configure.sh
@@ -4,6 +4,9 @@ source ./configs/.env
 echo "Installing psutil for zshrc..."
 pip3 install psutil
 
+echo "Create symboliking spaceship theme..."
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
+
 echo "Create symboliking .zshrc modified..."
 if [ -f "$ZSHRC_FILE" ]; then
   mv $ZSHRC_FILE $ZSHRC_FILE.orig
@@ -11,9 +14,6 @@ if [ -f "$ZSHRC_FILE" ]; then
 else
   ln -s $ZSHRC_FILE_UPDATED $ZSHRC_FILE
 fi
-
-echo "Create symboliking spaceship theme..."
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
 
 echo "Create symboliking .gitconfig..."
 if [ -f "$GIT_CONFIG_FILE" ]; then
@@ -25,7 +25,7 @@ fi
 
 echo "Create symboliking .alias..."
 if [ -f "$ALIAS_FILE" ]; then
-  mv $ALIAS_FILE_UPDATED $ALIAS_FILE.orig
+  mv $ALIAS_FILE $ALIAS_FILE.orig
   ln -s $ALIAS_FILE_UPDATED $ALIAS_FILE
 else
   ln -s $ALIAS_FILE_UPDATED $ALIAS_FILE
@@ -33,7 +33,7 @@ fi
 
 echo "Create symboliking .tmux.conf..."
 if [ -f "$TMUX_FILE" ]; then
-  mv $TMUX_FILE $ALIAS_FILE.orig
+  mv $TMUX_FILE $TMUX_FILE.orig
   ln -s $TMUX_FILE_UPDATED $TMUX_FILE
 else
   ln -s $TMUX_FILE_UPDATED $TMUX_FILE
