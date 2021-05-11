2763e555419f9c84941927f57c90406f496c9180
HallexCosta <hallex.costa@hotmail.com>
2021-05-10 21:30:05 -0300

    chore(bins): clone repo spaceship-theme with --depth=1

--- bins/spaceship-theme.sh
+++ bins/spaceship-theme.sh
@@ -1,6 +1,5 @@
 echo ""
 echo "Installing theme spaceship..."
-mkdir $ZSH_CUSTOM_DIR/themes/spaceship-prompt
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM_DIR/themes/spaceship-prompt"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM_DIR/themes/spaceship-prompt" --depth=1
 
 echo "Done!"
