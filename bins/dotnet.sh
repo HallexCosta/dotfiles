377726fce1682741006188ab733ce02e6982aca8
HallexCosta <hallex.costa@hotmail.com>
2021-05-29 00:38:22 -0300

    chore(bins): add dotnet installer

---
+++ bins/dotnet.sh
@@ -0,0 +1,12 @@
+echo "Downloading dotnet..."
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+echo "Installing dotnet SDK 5.0"
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-5.0
+
+echo "Removing source dotnet downloaded"
+rm packages-microsoft-prod.deb
